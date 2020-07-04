263adaae965b25e569f11d3dc0a3a95d5a9417c3
Angel Barusta <angel.barusta.95@gmail.com>
2020-07-04 15:58:47 -0500

    hooks_04072020_1558

--- src/components/NavDetailsPoke.js
+++ src/components/NavDetailsPoke.js
@@ -1,47 +1,33 @@
-import React, { Component } from "react";
-
+import React, { useState, Component } from "react";
 import { Menu, Segment } from "semantic-ui-react";
 import { InfoPokeData } from "./infoPokeData";
 
 import "./styles/DetallesPoke.css";
 
-export default class NavDetailsPoke extends Component {
-  state = { activeItem: "About" };
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-
-  render() {
-    const { activeItem } = this.state;
-    const { poke, evo } = this.props;
-    return (
-      <div className='NavDetallesPoke__Container'>
-        <Menu pointing secondary className='NavDetallesPoke__Container--Menu'>
-          <Menu.Item
-            name='About'
-            active={activeItem === "About"}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='Base Stats'
-            active={activeItem === "Base Stats"}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='Evolution'
-            active={activeItem === "Evolution"}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='Moves'
-            active={activeItem === "Moves"}
-            onClick={this.handleItemClick}
-          />
-        </Menu>
-
-        <Segment className='NavDetallesPoke__Container--SegmentDetails'>
-          <InfoPokeData poke={poke} mode={activeItem} evo={evo} />
-        </Segment>
-      </div>
-    );
-  }
-}
+const arrItem = [
+  { name: "About" },
+  { name: "Base Stats" },
+  { name: "Evolution" },
+  { name: "Moves" },
+];
+const NavDetailsPoke = ({ poke, evo }) => {
+  const [activeItem, setActiveItem] = useState("About");
+  const listItem = arrItem.map((s, i) => (
+    <Menu.Item
+      name={s.name}
+      active={activeItem === s.name}
+      onClick={(e, { name }) => setActiveItem(name)}
+    />
+  ));
+  return (
+    <div className='NavDetallesPoke__Container'>
+      <Menu pointing secondary className='NavDetallesPoke__Container--Menu'>
+        {listItem}
+      </Menu>
+      <Segment className='NavDetallesPoke__Container--SegmentDetails'>
+        <InfoPokeData poke={poke} mode={activeItem} evo={evo} />
+      </Segment>
+    </div>
+  );
+};
+export default NavDetailsPoke;
